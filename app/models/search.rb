59b615409d0530a70bb50c188ec35a5497d0f07d
Mohammed Rashid <morash.9222@gmail.com>
2015-06-05 01:58:54 +0100

    initial commit

---
+++ app/models/search.rb
@@ -0,0 +1,27 @@
+class Search
+
+	attr_reader :term
+
+	def to_partial_path
+		"searches/search"
+	end
+
+	def initialize options = {}
+		@term = options.fetch(:term, "")
+	end
+
+	def shouts
+		Shout.text_shouts.where(content_id: text_shouts)
+	
+	end
+
+	private
+
+	def text_shouts
+		TextShout.where("body LIKE?", search_term)
+	end 
+
+	def search_term 
+		"%#{term}%"
+	end
+end 
